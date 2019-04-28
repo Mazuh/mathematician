ced4005ffea23ac6657f63a2911fb341ecb32f66
mazuh <marcell-mz@hotmail.com>
2019-04-28 15:36:02 -0300

    expose more apis

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import evalPostfix from './evals';
+import { parseExpression } from './parser';
+import * as syntax from './syntax';
 
 export default {
   evalPostfix,
+  parseExpression,
+  ...syntax,
 };
